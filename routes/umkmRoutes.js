c599758916c643f7b006ecefd72426600ab0d219
humandkind-pro <fikrank161@gmail.com>
2026-05-10 18:37:51 +0800

    minor bug change #2

--- routes/umkmRoutes.js
+++ routes/umkmRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const umkmController = require('../controllers/UmkmController');
 const auth = require('../middleware/authMiddleware'); 
-const auth = require('../authMiddleware');     
 
 router.get('/', umkmController.getAllUmkm);
 router.post('/', auth, umkmController.createUmkm);
